31de743d198c0c6dd0daddbcd3b6eed8bafaae62
Paul Zaich <pzaich@gmail.com>
2012-07-31 11:28:46 -0700

    remove deleted files

--- db/migrate/20120730193441_add_slug_to_polls.rb
+++
@@ -1,6 +0,0 @@
-class AddSlugToPolls < ActiveRecord::Migration
-  def change
-  	add_column :polls, :slug, :string, :null => false
-  	add_index :polls, :slug
-  end
-end
--- db/migrate/20120731053201_model.rb
+++
@@ -1,7 +0,0 @@
-class Model < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
