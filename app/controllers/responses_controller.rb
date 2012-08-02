3f602245b4862f793d379907160c8dabdf159112
Paul Zaich <pzaich@gmail.com>
2012-08-01 22:29:49 -0700

    fixed responses

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,9 +1,4 @@
 class ResponsesController < ApplicationController
-  def show
-  end
-
-  def edit
-  end
 
   def new
   	@response = Response.new
@@ -14,15 +9,14 @@ class ResponsesController < ApplicationController
     @next_question = @question.poll.questions.where("id > ?", params[:question_id]).first
   	@response = Response.new(:question_id => params[:question_id], :answer_id => params[:answer_id])
     if @response.save
-      warn "*******************************************"
-      warn @next_question
-      if @next_question.nil?
-        redirect_to poll_path(@question.poll)
+      if @next_question.nil? 
+        redirect_to poll_path(@question.poll), :flash => {:success => "Survey Complete"}
       else
         redirect_to question_path(@next_question)
       end
     else
-      render question_path(@question), :flash => {:error => "Something went wrong, try again."}
+      warn '*************************'
+      redirect_to question_path(@question), :flash => {:error => "Update unsuccessful. Please try again."}
     end
   end
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -2,4 +2,6 @@ class Response < ActiveRecord::Base
   attr_accessible :answer_id, :question_id
   belongs_to :answer
   belongs_to :question
+
+  validates_presence_of :answer_id, :question_id
 end
